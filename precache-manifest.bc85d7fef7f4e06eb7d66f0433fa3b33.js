cf68a5e91142dc94eae7bf766eb7e6df90367ecc
osamaali <syed.osama.ali.96@gmail.com>
2020-06-17 19:44:00 +0500

    Updates

---
+++ precache-manifest.bc85d7fef7f4e06eb7d66f0433fa3b33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf42d3a88596b776f897f4c46a1e4db7",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "217e002153fc5adf4d63",
+    "url": "/react-portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "6150fcbb7d08d8dc3452",
+    "url": "/react-portfolio/static/css/main.c881ae80.chunk.css"
+  },
+  {
+    "revision": "217e002153fc5adf4d63",
+    "url": "/react-portfolio/static/js/2.3f43258e.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/react-portfolio/static/js/2.3f43258e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6150fcbb7d08d8dc3452",
+    "url": "/react-portfolio/static/js/main.e940768d.chunk.js"
+  },
+  {
+    "revision": "0d4edfcbb3e61ba4a975",
+    "url": "/react-portfolio/static/js/runtime-main.066a00c3.js"
+  }
+]);
